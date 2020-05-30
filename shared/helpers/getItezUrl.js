2a7f5c0105f20989fef8100a4155740a84932779
opensim4sd <66175998+opensim4sd@users.noreply.github.com>
2020-05-30 19:41:25 +0300

    Add files via upload

---
+++ shared/helpers/getItezUrl.js
@@ -0,0 +1,38 @@
+
+export const getItezUrl = ({ user, locale, url }) => {
+  if (!user && !locale) return url
+
+  const { activeFiat, btcMnemonicData, btcMultisigG2FAData, btcMultisigSMSData, btcMultisigUserData, btcData } = user
+
+  let getActuallyFiatAddress = [btcMnemonicData, btcMultisigG2FAData, btcMultisigSMSData, btcMultisigUserData, btcData].find(({ balance }) => balance > 0);
+
+  if (!getActuallyFiatAddress) {
+    getActuallyFiatAddress = btcData
+  }
+
+  const shieldingСomingFiat = /%7BDEFAULT_FIAT%7D/gi
+  const shieldingСomingLocale = /%7Blocale%7D/gi
+  const shieldingСomingAddress = /%7Bbtcaddress%7D/gi
+
+  const comingFiat = /{DEFAULT_FIAT}/gi
+  const comingLocale = /{locale}/gi
+  const comingAddress = /{btcaddress}/gi
+
+  let returned = url
+
+  if (url.includes('btcaddress')) {
+    returned = returned.replace(comingAddress, getActuallyFiatAddress.address)
+    returned = returned.replace(shieldingСomingAddress, getActuallyFiatAddress.address)
+  }
+  if (url.includes('DEFAULT_FIAT')) {
+    returned = returned.replace(comingFiat, activeFiat)
+    returned = returned.replace(shieldingСomingFiat, activeFiat)
+  }
+
+  if (url.includes('locale')) {
+    returned = returned.replace(comingLocale, locale)
+    returned = returned.replace(shieldingСomingLocale, locale)
+  }
+
+  return returned
+}
